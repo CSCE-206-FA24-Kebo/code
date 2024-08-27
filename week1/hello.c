3e05f029cfd18cfd627a7a925501962326ed3c05
David Kebo <davidkebo@tamu.edu>
2024-08-27 15:53:46 +0000

    Week 1

---
+++ week1/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // This is a comment
+    /*
+    Week 1 practice:
+    This program prints a hello world message
+    */
+    printf("Hello welcome to CSCE 206!\n");
+    return 0;
+}
